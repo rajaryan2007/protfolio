b930ef6afb8b98b733de688bf469bf4ceab6f6ba
rajaryan2007 <rajaryan150149@gmail.com>
2025-07-18 16:52:14 +0530

    add

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,12 +10,14 @@ const About = () => {
         <div className='w-full h-full border border-gray-700 bg-black-200 rounded-lg p-4 sm:p-7 flex flex-col gap-5'>
           <img src="assets/grid1.png" alt="Grid 1" className='rounded-lg object-cover w-full' />
           <p className='text-xl font-semibold text-white font-generalsans'>Hello</p>
-          <p className='text-[#afb0b6] text-base font-generalsans'>hello</p>
+          <p className='text-[#afb0b6] text-base font-generalsans'>my name is rajaryan from class 12th </p>
         </div>
 
         {/* Card 2 */}
         <div className='w-full h-full border border-gray-700 bg-black-200 rounded-lg p-4 sm:p-7 flex flex-col gap-5'>
           <img src="assets/grid2.png" alt="Grid 2" className='rounded-lg object-cover w-full' />
+           <p className='text-xl font-semibold text-white font-generalsans'></p>
+          <p className='text-[#afb0b6] text-base font-generalsans'> KNOW ABOUT REACT, THREE.JS and tailwindcss </p>
         </div>
 
         {/* Globe Card */}
@@ -29,8 +31,8 @@ const About = () => {
               globeImageUrl="	//unpkg.com/three-globe/example/img/earth-night.jpg"
               bumpImageUrl="/unpkg.com/three-globe/example/img/earth-topology.png" />
           </div>
-          <p className='text-xl font-semibold mb-2 text-white font-generalsans'>HELLO</p>
-          <p className='text-[#afb0b6] text-base font-generalsans'>HELLO</p>
+          <p className='text-xl font-semibold mb-2 text-white font-generalsans'>FROM</p>
+          <p className='text-[#afb0b6] text-base font-generalsans'>INDIA,DELHI</p>
           <button name='contact me' isBeam containerClass="w-full mt-10" />
 
         </div>
